a058bab8cdb6224c740edaf2cc9b6891fa6e5b50
Karthik Reddy <svb491@gmail.com>
2024-03-11 14:09:58 +0100

    Addtion fail

--- calculator.py
+++ calculator.py
@@ -2,7 +2,7 @@
 
 class Calculator:
     def add(self, x, y):
-        return
+        return """ no return statement"""
 
     """def subtract(self, x, y):
         return x - y
--- test_calculator.py
+++ test_calculator.py
@@ -3,7 +3,7 @@ from calculator import Calculator
 calculator = Calculator()
 
 
-def test_add(a, b, expected_result):
+def test(a, b, expected_result):
     result = calculator.add(a, b)
     if result == expected_result:
         print("Addition test passed")
@@ -11,4 +11,4 @@ def test_add(a, b, expected_result):
         raise ValueError("Addition test failed. Expected:", expected_result, "but got:", result)
 
 
-test_add(2, 3, 55)
+test(2, 3, 5)
