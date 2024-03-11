e8a18b6835a895d7cc2c85bfaac9fbd690d2aa97
Karthik Reddy <svb491@gmail.com>
2024-03-11 15:50:03 +0100

    now i am going to pass all the test cases by giving correct expected output.

--- test_calculator.py
+++ test_calculator.py
@@ -6,27 +6,27 @@ calculator = Calculator()
 def test_add():
     a = 2
     b = 3
-    expected_result = 4
+    expected_result = 5
     result = calculator.add(a, b)
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
 
 def test_sub():
     a = 5
     b = 2
-    expected_result = 2
+    expected_result = 3
     result = calculator.subtract(a, b)
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}" 
 
 def test_mult():
     a = 5
     b = 2
-    expected_result = 1
+    expected_result = 10
     result = calculator.multiply(a, b)
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
 
 def test_div():
     a = 4
     b = 2
-    expected_result = 4
+    expected_result = 2
     result = calculator.divide(a, b)
     assert result == expected_result, f"Expected: {expected_result}, but got: {result}"
